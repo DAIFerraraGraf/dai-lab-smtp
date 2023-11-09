da01d84a2b1a3e05dc12683e7380eb36de2481d2
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2023-11-09 12:00:54 +0100

    add smtp client base

--- dai-lab-smtp/src/SMTPClient/Message.java
+++ dai-lab-smtp/src/SMTPClient/Message.java
@@ -1,4 +1,52 @@
 package SMTPClient;
 
+import java.util.Date;
+
 public class Message {
+    private String fromSMTP;
+    private String toSMTP;
+    private String from;
+    private String to;
+    private Date date;
+    private String subject;
+    private String body;
+
+    public Message(String fromSMTP, String toSMTP, String from, String to, Date date, String subject, String body) {
+        this.fromSMTP = fromSMTP;
+        this.toSMTP = toSMTP;
+        this.from = from;
+        this.to = to;
+        this.date = date;
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getFromSMTP() {
+        return fromSMTP;
+    }
+
+    public String getToSMTP() {
+        return toSMTP;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
 }
--- dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
+++ dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
@@ -1,4 +1,57 @@
 package SMTPClient;
 
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 public class SMTPProtocol {
+    private final int SERVER_PORT = 1025;
+    private final String SERVER_ADDRESS = "0.0.0.0";
+
+    public void send() {
+
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
+
+            init(out, in);
+
+            String[] commands = sendHello(out, in);
+            for(String command : commands){
+                System.out.println(command);
+            }
+
+            quit(out, in);
+        } catch(IOException e){
+            System.out.println("Client: exc.: " + e);
+        }
+    }
+
+    private void init(BufferedWriter out, BufferedReader in) throws IOException {
+        if(in.readLine().contains("220"))
+            System.out.println("Client: connect successful");
+    }
+
+    private String[] sendHello(BufferedWriter out, BufferedReader in) throws IOException {
+        out.write("ehlo " + SERVER_ADDRESS + "\r\n");
+        out.flush();
+
+        String line;
+        String commands = "";
+        do {
+            line = in.readLine();
+            commands += line + " ";
+        } while (line.charAt(3) != ' ');
+        return commands.split("250");
+    }
+
+    private void quit(BufferedWriter out, BufferedReader in) throws IOException {
+        out.write("quit \r\n");
+        out.flush();
+        if(in.readLine().contains("221"))
+            System.out.println("Client: quit successful");
+    }
+
+
 }
+
