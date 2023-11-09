350b0eed5d9b11ce2ec4c9689dc452c81ce1ec6f
Andrea Graf <andrea.graf@heig-vd.ch>
2023-11-09 11:46:59 +0100

    Create Feature in Class group

--- dai-lab-smtp/src/SMTPClient/Group.java
+++ dai-lab-smtp/src/SMTPClient/Group.java
@@ -1,4 +1,23 @@
 package SMTPClient;
 
+import java.util.List;
+
 public class Group {
+    private String sender;
+    private List<String> emails;
+
+    public Group(List<String> emails) {
+        this.emails = emails;
+        if (!emails.isEmpty())
+            this.sender = emails.get(0);
+    }
+    public String getSender() {
+        return sender;
+    }
+    public List<String> getEmails() {
+        return emails;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
 }
