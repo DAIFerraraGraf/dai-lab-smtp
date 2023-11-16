6ec8b0b5050b1193a2d2f7fdc4674aa485371dfd
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2023-11-16 11:45:41 +0100

    adapt email sender

--- dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
+++ dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
@@ -9,11 +9,12 @@ public class SMTPProtocol {
     private final int SERVER_PORT = 1025;
     private final String SERVER_ADDRESS = "0.0.0.0";
 
-    public void sendEmail(List<Group> groups, List<Message> messages) {
+    public void sendEmail(Group group, Message message) {
 
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
              var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
              var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
+
 
             init(out, in);
 
@@ -22,10 +23,11 @@ public class SMTPProtocol {
                 System.out.println(command);
             }
 
-            sendFromToSMTP(out, in, "imposteur@gmail.com", "juju@gmail.com");
-            sendContent(out, in, "shakira@gmail.com", "juju@gmail.com", "April 1st, 2021", "shasha", "hello im shakira");
+            sendFromToSMTP(out, in, group);
+            sendContent(out, in, message);
 
             quit(out, in);
+
         } catch (IOException e) {
             System.out.println("Client: exc.: " + e);
         }
@@ -49,9 +51,9 @@ public class SMTPProtocol {
         return commands.split("250");
     }
 
-    private void sendFromToSMTP(BufferedWriter out, BufferedReader in, String from, String to) throws IOException {
+    private void sendFromToSMTP(BufferedWriter out, BufferedReader in, Group group) throws IOException {
         String line;
-        out.write("mail from:<" + from + ">\r\n");
+        out.write("mail from:<" + group.getSender() + ">\r\n");
         out.flush();
         if (!in.readLine().contains("250")) {
 //            throw "error";
@@ -59,7 +61,9 @@ public class SMTPProtocol {
             return;
         }
 
-        out.write("rcpt to:<" + to + ">\r\n");
+        for (String to : group.getRecievers()) {
+            out.write("rcpt to:<" + to + ">\r\n");
+        }
         out.flush();
         if (!in.readLine().contains("250")) {
 //            throw "error";
@@ -68,8 +72,7 @@ public class SMTPProtocol {
         }
     }
 
-    private void sendContent(BufferedWriter out, BufferedReader in, String from, String to, String date, String subject,
-                             String content) throws IOException {
+    private void sendContent(BufferedWriter out, BufferedReader in, Message message) throws IOException {
         out.write("data\r\n");
         out.flush();
         if (!in.readLine().contains("354")) {
@@ -77,11 +80,11 @@ public class SMTPProtocol {
             System.out.println("error in data");
             return;
         }
-        out.write("From: <" + from + ">\r\n");
-        out.write("To: <" + to + ">\r\n");
-        out.write("Date: " + date + "\r\n");
-        out.write("Subject: " + subject + "\r\n");
-        out.write("\r\n" + content + "\r\n");
+        out.write("From: <" + message.getFrom() + ">\r\n");
+        out.write("To: <" + message.getTo() + ">\r\n");
+        out.write("Date: " + message.getDate() + "\r\n");
+        out.write("Subject: " + message.getSubject() + "\r\n");
+        out.write("\r\n" + message.getBody() + "\r\n");
         out.write("\r\n.\r\n");
         out.flush();
 //        if (!in.readLine().contains("250")) {
