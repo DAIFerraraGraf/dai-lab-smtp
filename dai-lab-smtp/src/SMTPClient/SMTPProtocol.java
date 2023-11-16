f714a3328aa29ebb73836ca4d3365596cd331a79
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2023-11-16 11:15:12 +0100

    working smtp

--- dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
+++ dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
@@ -3,6 +3,7 @@ package SMTPClient;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class SMTPProtocol {
     private final int SERVER_PORT = 1025;
@@ -17,18 +18,21 @@ public class SMTPProtocol {
             init(out, in);
 
             String[] commands = sendHello(out, in);
-            for(String command : commands){
+            for (String command : commands) {
                 System.out.println(command);
             }
 
+            sendFromToSMTP(out, in, "imposteur@gmail.com", "juju@gmail.com");
+            sendContent(out, in, "shakira@gmail.com", "juju@gmail.com", new Date(), "subject", "content");
+
             quit(out, in);
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Client: exc.: " + e);
         }
     }
 
     private void init(BufferedWriter out, BufferedReader in) throws IOException {
-        if(in.readLine().contains("220"))
+        if (in.readLine().contains("220"))
             System.out.println("Client: connect successful");
     }
 
@@ -45,10 +49,51 @@ public class SMTPProtocol {
         return commands.split("250");
     }
 
+    private void sendFromToSMTP(BufferedWriter out, BufferedReader in, String from, String to) throws IOException {
+        String line;
+        out.write("mail from:<" + from + ">\r\n");
+        out.flush();
+        if (!in.readLine().contains("250")) {
+//            throw "error";
+            System.out.println("error in mailfrom");
+            return;
+        }
+
+        out.write("rcpt to:<" + to + ">\r\n");
+        out.flush();
+        if (!in.readLine().contains("250")) {
+//            throw "error";
+            System.out.println("error in mailto");
+            return;
+        }
+    }
+
+    private void sendContent(BufferedWriter out, BufferedReader in, String from, String to, Date date, String subject,
+                             String content) throws IOException {
+        out.write("data\r\n");
+//        if (!in.readLine().contains("354")) {
+////            throw "error";
+//            System.out.println("error in data");
+//            return;
+//        }
+        out.write("From: <" + from + ">\r\n");
+        out.write("To: <" + to + ">\r\n");
+        out.write("Date: " + date + "\r\n");
+        out.write("Subject: " + subject + "\r\n");
+        out.write(content + "\r\n");
+        out.write("\r\n.\r\n");
+        out.flush();
+//        if (!in.readLine().contains("250")) {
+////            throw "error";
+//            System.out.println("error in sending content");
+//            return;
+//        }
+    }
+
     private void quit(BufferedWriter out, BufferedReader in) throws IOException {
         out.write("quit \r\n");
         out.flush();
-        if(in.readLine().contains("221"))
+        if (in.readLine().contains("221"))
             System.out.println("Client: quit successful");
     }
 
