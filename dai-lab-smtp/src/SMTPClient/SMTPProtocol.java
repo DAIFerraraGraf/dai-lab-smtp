11c1c83346d3df95e8795d642f10b0b3a1d86edb
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2023-11-23 11:52:44 +0100

    refactor smtpprotocol

--- dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
+++ dai-lab-smtp/src/SMTPClient/SMTPProtocol.java
@@ -3,39 +3,37 @@ package SMTPClient;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.List;
+
 
 public class SMTPProtocol {
     private final int SERVER_PORT = 1025;
     private final String SERVER_ADDRESS = "0.0.0.0";
 
     public void sendEmail(Group group, Message message) {
-
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
              var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
              var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
 
-
-            init(out, in);
+            init(in);
 
             String[] commands = sendHello(out, in);
-            for (String command : commands) {
-                System.out.println(command);
-            }
-
-            sendFromToSMTP(out, in, group);
-            sendContent(out, in, message);
-
+            sendCommand(out, in, mailFrom(group.getSender()));
+            sendCommand(out, in, rcptTo(group.getRecievers().get(0)));
+            sendCommand(out, in, "data");
+            sendCommand(out, in, content(message.getFrom(), message.getTo(), message.getDate(), message.getSubject(), message.getBody()));
             quit(out, in);
 
         } catch (IOException e) {
-            System.out.println("Client: exc.: " + e);
+            throw new RuntimeException("Error while sending email", e);
         }
     }
 
-    private void init(BufferedWriter out, BufferedReader in) throws IOException {
-        if (in.readLine().contains("220"))
-            System.out.println("Client: connect successful");
+    private void init(BufferedReader in) throws IOException {
+        if (!in.readLine().contains("220")) {
+            throw new RuntimeException("Error while initializing connection");
+        }
     }
 
     private String[] sendHello(BufferedWriter out, BufferedReader in) throws IOException {
@@ -51,94 +49,42 @@ public class SMTPProtocol {
         return commands.split("250");
     }
 
-    private void sendFromToSMTP(BufferedWriter out, BufferedReader in, Group group) throws IOException {
-        String line;
-        out.write("mail from:<" + group.getSender() + ">\r\n");
-        out.flush();
-        if (!in.readLine().contains("250")) {
-//            throw "error";
-            System.out.println("error in mailfrom");
-            return;
-        }
-
-        for (String to : group.getRecievers()) {
-            out.write("rcpt to:<" + to + ">\r\n");
-            out.flush();
-            if (!in.readLine().contains("250")) {
-//            throw "error";
-                System.out.println("error in mailto");
-                return;
-            }
-        }
-
-    }
-
-    private void sendContent(BufferedWriter out, BufferedReader in, Message message) throws IOException {
-        out.write("data\r\n");
-        out.flush();
-        if (!in.readLine().contains("354")) {
-//            throw "error";
-            System.out.println("error in data");
-            return;
-        }
-        out.write("From: <" + message.getFrom() + ">\r\n");
-        out.write("To: <" + message.getTo() + ">\r\n");
-        out.write("Date: " + message.getDate() + "\r\n");
-        out.write("Subject: " + message.getSubject() + "\r\n");
-        out.write("\r\n" + message.getBody() + "\r\n");
-        out.write("\r\n.\r\n");
-        out.flush();
-//        if (!in.readLine().contains("250")) {
-////            throw "error";
-//            System.out.println("error in sending content");
-//            return;
-//        }
-    }
-
-    private void quit(BufferedWriter out, BufferedReader in) throws IOException {
-        out.write("quit \r\n");
-        out.flush();
-        if (in.readLine().contains("221"))
-            System.out.println("Client: quit successful");
-    }
-
-
-}
-
-/*
-
-public class SMTPProtocol {
-    private final int SERVER_PORT = 1025;
-    private final String SERVER_ADDRESS = "0.0.0.0";
-
-    public void sendEmail(Group group, Message message) {
-        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-             var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-
-            sendCommand(out, in, "ehlo " + SERVER_ADDRESS);
-            sendCommand(out, in, "mail from:<" + group.getSender() + ">");
-            group.getRecievers().forEach(receiver -> sendCommand(out, in, "rcpt to:<" + receiver + ">"));
-            sendCommand(out, in, "data");
-            sendCommand(out, in, "From: <" + message.getFrom() + ">\r\nTo: <" + message.getTo() + ">\r\nDate: " + message.getDate() + "\r\nSubject: " + message.getSubject() + "\r\n\r\n" + message.getBody() + "\r\n.\r\n");
-            sendCommand(out, in, "quit");
-
-        } catch (IOException e) {
-            throw new RuntimeException("Error while sending email", e);
-        }
-    }
-
     private void sendCommand(BufferedWriter out, BufferedReader in, String command) {
         try {
             out.write(command + "\r\n");
             out.flush();
             String response = in.readLine();
-            if (!response.contains("250") && !response.contains("220") && !response.contains("354")) {
+            if (!response.contains("250") && !response.contains("220") && !response.contains("354") && !response.contains("221")) {
                 throw new RuntimeException("Error in command: " + command);
             }
         } catch (IOException e) {
             throw new RuntimeException("Error while sending command: " + command, e);
         }
     }
-}*/
+
+    private void quit(BufferedWriter out, BufferedReader in) {
+        try {
+            out.write("quit\r\n");
+            out.flush();
+            String response = in.readLine();
+            if (!response.contains("221")) {
+                throw new RuntimeException("Error in command: quit");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error while sending command: quit", e);
+        }
+    }
+
+    private String mailFrom(String sender) {
+        return "mail from:<" + sender + ">";
+    }
+
+    private String rcptTo(String receiver) {
+        return "rcpt to:<" + receiver + ">";
+    }
+
+    private String content(String from, String to, Date date, String subject, String body) {
+        return "From: <" + from + ">\r\nTo: <" + to + ">\r\nDate: " + date + "\r\nSubject: " + subject + "\r\n\r\n" + body + "\r\n.";
+    }
+}
 
