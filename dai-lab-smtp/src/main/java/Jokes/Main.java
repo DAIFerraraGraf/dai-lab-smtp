f7110de2d8da00539cefb0a0bc520e583a2c2dc4
Andrea Graf <andrea.graf@heig-vd.ch>
2023-11-24 09:40:59 +0100

    Modify the display in consol of json structure

--- dai-lab-smtp/src/main/java/Jokes/Main.java
+++ dai-lab-smtp/src/main/java/Jokes/Main.java
@@ -28,7 +28,7 @@ public class Main {
         String ANSI_Green = "\u001B[32m";
         String ANSI_Magenta = "\u001B[35m";
         String ANSI_RESET = "\u001B[0m";
-        return "{\n" +
+        return "\n{\n" +
                 ANSI_Magenta  +"  \"messages\" "+  ANSI_RESET +": [\n" +
                 "    {\n" +
                 ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
@@ -37,10 +37,14 @@ public class Main {
                 "    {\n" +
                 ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
                 ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
-                "    }\n" +
+                "    },\n" +
                 "    ...\n" +
+                "    {\n" +
+                ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
+                ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
+                "    }\n" +
                 "  ]\n" +
-                "}";
+                "}\n";
     }
 
 }
