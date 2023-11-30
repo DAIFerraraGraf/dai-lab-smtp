1af0f776ecae34b64c527eb26023eb3adb9c1309
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2023-11-30 10:45:31 +0100

    refactor

---
+++ dai-lab-smtp/src/main/java/Jokes/CommandLine.java
@@ -0,0 +1,32 @@
+package Jokes;
+
+public class CommandLine {
+    public static String getHelp() {
+        String ANSI_Green = "\u001B[32m";
+        String ANSI_RESET = "\u001B[0m";
+        return ANSI_Green + "Usage: java -jar <SpammerEmail.jar> <email_addresses_file.txt> <messages_file.json>\n" +
+                "Use --json to display the JSON structure"+ ANSI_RESET;
+    }
+    public static String getJSONStructure() {
+        String ANSI_Green = "\u001B[32m";
+        String ANSI_Magenta = "\u001B[35m";
+        String ANSI_RESET = "\u001B[0m";
+        return "\n{\n" +
+                ANSI_Magenta  +"  \"messages\" "+  ANSI_RESET +": [\n" +
+                "    {\n" +
+                ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
+                ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
+                "    },\n" +
+                "    {\n" +
+                ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
+                ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
+                "    },\n" +
+                "    ...\n" +
+                "    {\n" +
+                ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
+                ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
+                "    }\n" +
+                "  ]\n" +
+                "}\n";
+    }
+}
--- dai-lab-smtp/src/main/java/Jokes/Main.java
+++ dai-lab-smtp/src/main/java/Jokes/Main.java
@@ -1,12 +1,12 @@
 package Jokes;
-//test
+
 public class Main {
     public static void main(String[] args) {
         if(args[0].equals("--json")){
-            System.out.println(getJSONStructure());
+            System.out.println(CommandLine.getJSONStructure());
             System.exit(0);
         }else if(args[0].equals("--help") || args.length != 2){
-            System.out.println(getHelp());
+            System.out.println(CommandLine.getHelp());
             System.exit(0);
         }
         // Créer un client SMTP.
@@ -17,34 +17,6 @@ public class Main {
 
         // Envoyer les messages.
         client.sendEmails();
-    }
-    public static String getHelp() {
-        String ANSI_Green = "\u001B[32m";
-        String ANSI_RESET = "\u001B[0m";
-        return ANSI_Green + "Usage: java -jar <SpammerEmail.jar> <email_addresses_file.txt> <messages_file.json>\n" +
-                          "Use --json to display the JSON structure"+ ANSI_RESET;
-    }
-    public static String getJSONStructure() {
-        String ANSI_Green = "\u001B[32m";
-        String ANSI_Magenta = "\u001B[35m";
-        String ANSI_RESET = "\u001B[0m";
-        return "\n{\n" +
-                ANSI_Magenta  +"  \"messages\" "+  ANSI_RESET +": [\n" +
-                "    {\n" +
-                ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
-                ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
-                "    },\n" +
-                "    {\n" +
-                ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
-                ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
-                "    },\n" +
-                "    ...\n" +
-                "    {\n" +
-                ANSI_Magenta  +"      \"Subject\" " + ANSI_RESET +":" + ANSI_Green +" \"<subject>\""+ ANSI_RESET +",\n" +
-                ANSI_Magenta  +"      \"Body\"" + ANSI_RESET + ":" + ANSI_Green +" \"<body>\"\n" + ANSI_RESET +
-                "    }\n" +
-                "  ]\n" +
-                "}\n";
     }
 
 }
